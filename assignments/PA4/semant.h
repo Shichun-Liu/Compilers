eca0f624f60d3b154a89cf3b96f60a1c6577eb91
LiuShichun <20307130111@fudan.edu.cn>
2023-05-05 19:54:11 +0800

    formal

--- assignments/PA4/semant.h
+++ assignments/PA4/semant.h
@@ -2,60 +2,56 @@
 #define SEMANT_H_
 
 #include <assert.h>
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+
 #include "cool-tree.h"
+#include "list.h"
 #include "stringtab.h"
 #include "symtab.h"
-#include "list.h"
 
 #define TRUE 1
 #define FALSE 0
 
 class ClassTable;
-typedef ClassTable *ClassTableP;
+typedef ClassTable* ClassTableP;
 
 // This is a structure that may be used to contain the semantic
 // information such as the inheritance graph.  You may use it or not as
 // you like: it is only here to provide a container for the supplied
 // methods.
 
-
-
-
 class ClassTable {
 private:
-  int semant_errors;
-  void install_basic_classes();
-  ostream& error_stream;
-  // my add
-  std::unordered_map<Symbol, Class_> class_table;
-  std::unordered_map<Symbol, Symbol> inhert_graph;
-  // add end
+    int semant_errors;
+    void install_basic_classes();
+    ostream& error_stream;
+	
+    // TODO: acyclic inhert
+    std::unordered_map<Symbol, Class_> class_table;
+    std::unordered_map<Symbol, Symbol> inhert_graph;
 
 public:
-  ClassTable(Classes);
-  int errors() { return semant_errors; }
-  ostream& semant_error();
-  ostream& semant_error(Class_ c);
-  ostream& semant_error(Symbol filename, tree_node *t);
-  // my add 
-  void add_to_class_table(Class_ c);
-  bool check_acyclic_main_nodefine();
-  Class_ get_class(Symbol s);
-  Symbol get_same_method_parent(Symbol child, Symbol name);
-  bool check_method(Symbol s1, Symbol s2, Symbol name);
-  bool is_class_exit(Symbol s);
-  bool is_sub_class(Symbol s1, Symbol s2);
-  Symbol lub(Symbol s1, Symbol s2);
-  Symbol get_parents(Symbol s, std::vector<Symbol> &v);
-  Formals get_formals(Symbol name, Symbol method);
-  Symbol get_return_type(Symbol name, Symbol method);
-  bool check(Formals formals, std::vector<Symbol> return_type);
-
+    ClassTable(Classes);
+    int errors() { return semant_errors; }
+    ostream& semant_error();
+    ostream& semant_error(Class_ c);
+    ostream& semant_error(Symbol filename, tree_node* t);
+    // my add
+    void add_to_class_table(Class_ c);
+    bool check_acyclic_main_nodefine();
+    Class_ get_class(Symbol s);
+    Symbol get_same_method_parent(Symbol child, Symbol name);
+    bool check_method(Symbol s1, Symbol s2, Symbol name);
+    bool is_class_exit(Symbol s);
+    bool is_sub_class(Symbol s1, Symbol s2);
+    Symbol lub(Symbol s1, Symbol s2);
+    Symbol get_parents(Symbol s, std::vector<Symbol>& v);
+    Formals get_formals(Symbol name, Symbol method);
+    Symbol get_return_type(Symbol name, Symbol method);
+    bool check(Formals formals, std::vector<Symbol> return_type);
 };
-
-
 
 #endif
